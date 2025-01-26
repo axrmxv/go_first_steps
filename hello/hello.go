c78e0125023749b4a7b57078fb6916cb9f54f70e
axkxd <altermaks375@gmail.com>
2025-01-26 16:22:14 +0300

    init

---
+++ hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Задайте свойства предопределенного Logger, включая
+	// префикс записи журнала и флаг для отключения печати
+	// времени, исходного файла и номера строки.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Список имен.
+	names := []string{"Gloria", "Samantha", "Darlin"}
+
+	// Запрос приветственных сообщений для имен.
+	messages, err := greetings.Hellos(names)
+	// Если была возвращена ошибка, вывести ее на консоль и
+	// выйти из программы.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Если ошибок не было, вывести возвращенную map
+	// сообщений на консоль.
+	fmt.Println(messages)
+}
